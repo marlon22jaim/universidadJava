731109bd2805b305c27fee8f2b9946402c91c5a5
MARLON JAIMES <andres_jr19hotmail.com>
2022-11-29 08:30:25 -0500

    bloques de inicialización part1

---
+++ Fundamentos/BloquesInicializacion/src/domain/Persona.java
@@ -0,0 +1,30 @@
+package domain;
+
+/**
+ *
+ * @author Marlon
+ */
+public class Persona {
+
+    private final int idPersona;
+    private static int contadorPersonas;
+
+    /**
+     * Descripción: BLoque de inicialización estático (se ejecuta al iniciar el
+     * programa antes del constructor) Se ejecuta la primera vez que se carga en
+     * memoria esta clase
+     */
+    static {
+        System.out.println("Ejecución Bloque estatico");
+        ++Persona.contadorPersonas;
+    }
+
+    /**
+     * Descripcion: Bloque de inicialización NO estatico (se ejecuta antes del
+     * constructor) Se ejecuta cada vez que se crea un nuevo objeto
+     */
+    {
+        System.out.println("Ejecución de bloque no estatico");
+        this.idPersona = Persona.contadorPersonas++;
+    }
+}
